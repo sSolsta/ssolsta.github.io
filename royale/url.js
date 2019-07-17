3014bd27ebd0e612294e37c57ac498969ab27d01
sSolsta <52147022+sSolsta@users.noreply.github.com>
2019-07-17 14:13:05 +1000

    Update url.js

--- royale/url.js
+++ royale/url.js
@@ -2,6 +2,6 @@ print("sSolsta's Menu/Music Mod is Active");
 var LOBBY_MUSIC_URL = "https://ssolsta.github.io/royale/music/lobby.mp3";
 var MENU_MUSIC_URL = "https://ssolsta.github.io/royale/music/menu.mp3";
 var SKIN_MUSIC_URL = {
-    420: "lol fuck skin music"
+    420: "this has to be here, hopefully there won't be a 421st skin"
 };
 print("loading url.js finished");
