2eb722ca5b2eb8aa02f672852c8ae35850f54999
sSolsta <52147022+sSolsta@users.noreply.github.com>
2019-07-20 13:56:12 +1000

    Update url.js

--- royale/url.js
+++ royale/url.js
@@ -2,7 +2,7 @@ print("sSolsta's Menu/Music Mod is Active");
 var LOBBY_MUSIC_URL = "https://ssolsta.github.io/royale/music/lobby.mp3";
 var MENU_MUSIC_URL = "https://ssolsta.github.io/royale/music/menu.mp3";
 var SKIN_MUSIC_URL = {
-    29: "https://ssolsta.github.io/royale/clubstep music.mp3",
-    //420: "this has to be here, hopefully there won't be a 421st skin"
+    //29: "https://ssolsta.github.io/royale/clubstep music.mp3",
+    420: "this has to be here, hopefully there won't be a 421st skin"
 };
 print("loading url.js finished");
