aaf96b7c480964de5b36db7cda32d775c7b887b8
sSolsta <52147022+sSolsta@users.noreply.github.com>
2019-07-20 13:56:03 +1000

    Update url.js

--- royale/url.js
+++ royale/url.js
@@ -3,6 +3,6 @@ var LOBBY_MUSIC_URL = "https://ssolsta.github.io/royale/music/lobby.mp3";
 var MENU_MUSIC_URL = "https://ssolsta.github.io/royale/music/menu.mp3";
 var SKIN_MUSIC_URL = {
     29: "https://ssolsta.github.io/royale/clubstep music.mp3",
-    420: "this has to be here, hopefully there won't be a 421st skin"
+    //420: "this has to be here, hopefully there won't be a 421st skin"
 };
 print("loading url.js finished");
