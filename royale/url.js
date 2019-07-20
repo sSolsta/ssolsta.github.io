1102a7cd6f141d308f18394decf8e534096c2ad0
sSolsta <52147022+sSolsta@users.noreply.github.com>
2019-07-20 13:42:00 +1000

    Update url.js

--- royale/url.js
+++ royale/url.js
@@ -2,7 +2,7 @@ print("sSolsta's Menu/Music Mod is Active");
 var LOBBY_MUSIC_URL = "https://ssolsta.github.io/royale/music/lobby.mp3";
 var MENU_MUSIC_URL = "https://ssolsta.github.io/royale/music/menu.mp3";
 var SKIN_MUSIC_URL = {
-    29: "https://cdn.discordapp.com/attachments/600397227327881247/601979225498583080/clubstep_music.mp3"
+    29: "https://api.allorigins.win/raw?url=https://cdn.discordapp.com/attachments/600397227327881247/601979225498583080/clubstep_music.mp3"
     420: "this has to be here, hopefully there won't be a 421st skin"
 };
 print("loading url.js finished");
