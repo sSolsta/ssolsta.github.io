c3d1e41de8c1cddebb45eeb31f77816ac5f24f39
sSolsta <52147022+sSolsta@users.noreply.github.com>
2019-10-22 16:45:28 +1100

    Update url.js

--- royale/url.js
+++ royale/url.js
@@ -1,16 +1,11 @@
 print("sSolsta's Menu/Music Mod is Active");
 var LOBBY_MUSIC_URL = "https://ssolsta.github.io/royale/music/lobby.mp3";
 var MENU_MUSIC_URL = "https://ssolsta.github.io/royale/music/menu.mp3";
-var SKINCOUNT = 1;
 var SKIN_MUSIC_URL = {};
+var TILE_ANIMATION = {};
+var OBJ_ANIMATION = {};
 (function() {
-    $.ajax({
-        url: "assets.json",
-        success: function (result) {
-            assetData = result;
-        },
-        async: false
-    });
+    var assetData = resources["assets.json"];
     if (assetData.skins.count != undefined)
         SKINCOUNT=assetData.skins.count;
     for (i in assetData.skins.properties) {
@@ -18,6 +13,24 @@ var SKIN_MUSIC_URL = {};
         var music = prop[i].music;
         if (music != undefined)
             SKIN_MUSIC_URL[prop[i].id] = music;
+    }
+    if (assetData.tileAnim) {
+        for (var anim of assetData.tileAnim) {
+            var obj = {};
+            obj.tiles = anim.tiles;
+            obj.delay = anim.delay;
+            obj.tilesets = anim.tilesets || [];
+            TILE_ANIMATION[anim.startTile] = obj;
+        }
+    }
+    if (assetData.objAnim) {
+        for (var anim of assetData.objAnim) {
+            var obj = {};
+            obj.tiles = anim.tiles;
+            obj.delay = anim.delay;
+            obj.tilesets = anim.tilesets || [];
+            OBJ_ANIMATION[anim.startTile] = obj;
+        }
     }
 })();
 print("loading url.js finished");
